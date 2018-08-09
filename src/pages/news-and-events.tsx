a11725958abeac6b7c0969765beb1018706f2dc7
Nathan Smith <nathan@nathansmith.io>
2018-08-08 21:17:07 -0400

    Add the event

--- src/pages/news-and-events.tsx
+++ src/pages/news-and-events.tsx
@@ -1,6 +1,55 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import { css } from 'react-emotion';
 import Header from '../components/Header';
+import PageContainer from '../components/PageContainer';
+import Footer from '../components/Footer';
 
-export default function NewsAndEventsPage() {
-  return <Header />;
+export const query = graphql`
+  query NewsAndEventsQuery {
+    eventImage: file(relativePath: { regex: "/alumni.ADX.jpg/" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+export default function NewsAndEventsPage({ data }: any) {
+  const image = data.eventImage.childImageSharp.fluid;
+  return (
+    <>
+      <Header />
+      <PageContainer title="Events">
+        <div
+          className={css`
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+          `}
+        >
+          <div>
+            <header>
+              <span>2018</span>
+              <h3>Fall</h3>
+            </header>
+            <p>
+              The Daily Bruin Alumni Association plans to host its first reunion
+              at UCLA in the fall. This will be our formal group launch and will
+              offer a chance to gather in Westwood and hear about the current
+              staff. Stay tuned for more developments!
+            </p>
+          </div>
+          <Img fluid={image} />
+        </div>
+      </PageContainer>
+      <Footer
+        githubName="alumni"
+        developers={['Nathan Smith']}
+        copyrightYear={2018}
+      />
+    </>
+  );
 }
