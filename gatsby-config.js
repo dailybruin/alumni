298f72d3f6ea79d4b50d2fa913490cc09b5cdb7b
Nathan Smith <nathan@nathansmith.io>
2018-08-20 22:28:34 -0400

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@ module.exports = {
     'gatsby-plugin-emotion',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-28181852-27',
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     'gatsby-plugin-twitter',
     {
       resolve: 'gatsby-source-filesystem',
