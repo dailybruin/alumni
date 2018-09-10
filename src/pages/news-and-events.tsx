da546539f136a2d620b3b97a038e271238bc76bb
Michael <myque@live.com>
2018-09-09 18:03:16 -0700

    Update reunion event text. Closes #22.
    Removed grid layout and smaller image as these additions
    were affecting usability on mobile

--- src/pages/news-and-events.tsx
+++ src/pages/news-and-events.tsx
@@ -8,7 +8,7 @@ export const query = graphql`
   query NewsAndEventsQuery {
     eventImage: file(relativePath: { regex: "/alumni.ADX.jpg/" }) {
       childImageSharp {
-        fluid {
+        fluid(maxHeight: 320, cropFocus: NORTH) {
           ...GatsbyImageSharpFluid
         }
       }
@@ -19,26 +19,43 @@ export const query = graphql`
 export default function NewsAndEventsPage({ data }: any) {
   const image = data.eventImage.childImageSharp.fluid;
   return (
-    <PageContainer title="Events">
+    <PageContainer title="Events" featureImage={image}>
       <div
         className={css`
           display: grid;
-          grid-template-columns: 1fr 1fr;
+          grid-template-columns: 1fr;
         `}
       >
         <div>
           <header>
             <span>2018</span>
-            <h3>Fall</h3>
+            <h3>October 5-6</h3>
           </header>
           <p>
-            Save the date: The Daily Bruin Alumni Network will host its first
-            reunion and general meeting at UCLA in the fall. This will be our
-            formal group launch and will offer a chance to gather in Westwood
-            and hear about the current staff. Stay tuned for an RSVP link.
+            The Daily Bruin Alumni Network is excited to host its
+            first alumni reunion on the UCLA campus this fall! 
+          </p>
+          <p>
+            <a target="_blank" href="http://giving.ucla.edu/2018DailyBruinReunion">Please RSVP here.</a>
+          </p>
+          <hr/>
+          <p>
+            Join us Friday afternoon for breakout sessions with past and present Daily Bruin
+            staffers, along with tours of campus and the newsroom. A full daytime schedule to come.
+          </p>
+          <p>
+            That evening, we will gather in the James West Alumni Center for a reception and
+            dinner program, which will feature the Daily Bruin Alumni Network's first Distinguished
+            Alumni honoree and updates from the organizational leadership and fundraising committees.
+          </p>
+          <p>
+            Join one of our meet-ups on Saturday, either to tailgate the UCLA football
+            game in Pasadena or watch together in Westwood. Location specifics to come.
+          </p>
+          <p>
+            Questions? Want to see who else is coming? <a target="_blank" href="http://www.facebook.com/events/315031935913662">Check out our Facebook event.</a>
           </p>
         </div>
-        <Img fluid={image} />
       </div>
     </PageContainer>
   );
