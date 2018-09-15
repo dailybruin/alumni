baea2dac4f12316227d0f726f50e075c5c2fd384
Nathan Smith <nathan@nathansmith.io>
2018-09-15 11:38:04 -0400

    Update event page (#36)
    * Update text
    
    * Add picture
    
    * color

--- src/pages/news-and-events.tsx
+++ src/pages/news-and-events.tsx
@@ -13,13 +13,22 @@ export const query = graphql`
         }
       }
     }
+    scheduleImage: file(relativePath: { regex: "/alumni-schedule.png/" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
   }
 `;
 
 export default function NewsAndEventsPage({ data }: any) {
-  const image = data.eventImage.childImageSharp.fluid;
+  const eventImage = data.eventImage.childImageSharp.fluid;
+  const scheduleImage = data.scheduleImage.childImageSharp.fluid;
+
   return (
-    <PageContainer title="Events" featureImage={image}>
+    <PageContainer title="Events" featureImage={eventImage}>
       <div
         className={css`
           display: grid;
@@ -32,28 +41,46 @@ export default function NewsAndEventsPage({ data }: any) {
             <h3>October 5-6</h3>
           </header>
           <p>
-            The Daily Bruin Alumni Network is excited to host its
-            first alumni reunion on the UCLA campus this fall! 
+            The Daily Bruin Alumni Network is excited to host its first alumni
+            reunion on the UCLA campus this fall!
           </p>
           <p>
-            <a href="http://giving.ucla.edu/2018DailyBruinReunion">Please RSVP here.</a>
+            <a href="http://giving.ucla.edu/2018DailyBruinReunion">
+              Please RSVP here.
+            </a>
           </p>
-          <hr/>
+          <hr />
           <p>
-            Join us Friday afternoon for breakout sessions with past and present Daily Bruin
-            staffers, along with tours of campus and the newsroom. A full daytime schedule to come.
+            Join us Friday afternoon for an open newsroom and breakout sessions
+            with past and present Daily Bruin staffers. Panels will include
+            discussions with Los Angeles Times reporters who are adjusting to a
+            new office and new ownership. The current managing team will discuss
+            the latest news around campus and how things are going at The Bruin.
+            For students, sessions will include a careers panel.
+          </p>
+          <p>See the full schedule outlined below:</p>
+          <Img
+            fluid={scheduleImage}
+            className={css`
+              margin-bottom: 1.45rem;
+            `}
+          />
+          <p>
+            That evening, we will gather in the James West Alumni Center for a
+            reception and dinner program, which will feature the Daily Bruin
+            Alumni Network's first Distinguished Alumni honoree and updates from
+            the organizational leadership and fundraising committees.
           </p>
           <p>
-            That evening, we will gather in the James West Alumni Center for a reception and
-            dinner program, which will feature the Daily Bruin Alumni Network's first Distinguished
-            Alumni honoree and updates from the organizational leadership and fundraising committees.
+            Join one of our meet-ups on Saturday, either to tailgate the UCLA
+            football game in Pasadena or watch together in Westwood. Location
+            specifics to come.
           </p>
           <p>
-            Join one of our meet-ups on Saturday, either to tailgate the UCLA football
-            game in Pasadena or watch together in Westwood. Location specifics to come.
-          </p>
-          <p>
-            Questions? Want to see who else is coming? <a href="http://www.facebook.com/events/315031935913662">Check out our Facebook event.</a>
+            Questions? Want to see who else is coming?{' '}
+            <a href="http://www.facebook.com/events/315031935913662">
+              Check out our Facebook event.
+            </a>
           </p>
         </div>
       </div>
