d34b9f6300ce2ca4758d9d7b1ff33c75312982d3
Richard <richbluehill@gmail.com>
2018-10-22 19:32:45 -0700

    fixed issue 49, 53, 56

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -30,122 +30,92 @@ export default function ContactPage({ data }) {
   return (
     <PageContainer title="About" featureImage={featureImage}>
       <p>
-        The Daily Bruin Alumni Network (the "DBAN") will actively engage the
-        alumni of the Daily Bruin to serve as a robust community for all former
-        staff members, in support of each other and The Bruin. The DBAN will
-        provide funding, mentorship and professional development programs for
-        aspiring student-journalists at UCLA, ensuring the excellence and
-        longevity of The Bruin. The DBAN will encourage students to reach beyond
-        UCLA, supporting community programs to advance news literacy and the
+        Welcome to the official site of the Daily Bruin Alumni Network, a group
+        devoted to supporting alumni of the UCLA Daily Bruin and student journalism
+        at the university. The organization is an official affinity network of the
+        UCLA Alumni Association.
+      </p>
+      <h2>Mission Statement</h2>
+      <p>
+        The Daily Bruin Alumni Network (the "DBAN") will actively engage the alumni of
+        the Daily Bruin to serve as a robust community for all former staff members,
+        in support of each other and The Bruin. The DBAN will provide funding, mentorship
+        and professional development programs for aspiring student-journalists at UCLA,
+        ensuring the excellence and longevity of The Bruin. The DBAN will encourage students
+        to reach beyond UCLA, supporting community programs to advance news literacy and the
         free press.
       </p>
-      <h2>Program Components</h2>
-      <h3>Alumni Engagement</h3>
-      <ul>
-        <li>
-          A governing board consisting of Daily Bruin alumni, the current
-          student alumni relations director and current student editor in chief
-          to oversee, manage and promote the DBAN.
-        </li>
-        <li>
-          An annual on-campus general meeting and reunion, featuring speakers,
-          career panels and receptions for Daily Bruin alumni from all
-          professions, to strengthen bonds among each other and with the current
-          students.
-        </li>
-        <li>
-          A dedicated DBAN website, as well as active social media engagement,
-          including private Facebook and LinkedIn groups, for our alumni in all
-          professions to network, to reconnect, to promote our events and to
-          share general updates and career opportunities.
-        </li>
-      </ul>
-      <h3>Mentorship and Development</h3>
-      <ul>
-        <li>
-          Pro bono advisory workshops and newspaper critiques offered by alumni
-          practicing journalists and media professionals for current Daily Bruin
-          students and staff.
-        </li>
-        <li>
-          Mentorship program and career guidance by alumni in journalism and
-          related fields.
-        </li>
-        <li>
-          Scholarship to fund original student journalism of global reach and
-          impact.
-        </li>
-        <li>
-          Work with academic departments like Communications Studies and English
-          to bolster professional advising and develop journalism courses for
-          student-journalists.
-        </li>
-      </ul>
-      <h3>Community Engagement and Service</h3>
-      <ul>
-        <li>
-          The Daily Bruin News Literacy Project: With alumni funding and
-          support, Daily Bruin advisors and staffers will regularly give back to
-          the community by going to local middle schools and high schools to
-          give workshops on the freedom of the press, journalism's role in
-          democracy and general news literacy.
-        </li>
-      </ul>
-      <h3>Alumni Funding/Endowment</h3>
+      <h2>History</h2>
       <p>
-        The Daily Bruin has had to undergo significant changes in recent years
-        due to steadily- declining advertising revenue. To maintain daily
-        publication, The Bruin has had to seek funding from student fees in 2009
-        and 2016. To limit rent costs, the Kerckhoff Hall office was reduced to
-        about half its previous size in 2017. The full-time student media
-        advisor position was eliminated, and the rest of the professional staff
-        has been reduced to just three positions. Editor stipends have fallen
-        sharply, requiring many staff members to take additional jobs.
+        The Daily Bruin Alumni Network's roots can be traced to 2016, when Lawrence Ma ('95)
+        connected with 2016-17 Editor-in-Chief Tanner Walters ('18). Lawrence had earmarked
+        a portion of his university donations for the Daily Bruin through UCLA Student Affairs,
+        which transfers the money to ASUCLA and into The Bruin's independent finances.
       </p>
       <p>
-        The future of journalism depends on the continued success of college
-        newspapers like the Daily Bruin, training the next generation of
-        journalists and media professionals. Here are the ways the DBAN and its
-        members can financially support the Daily Bruin:
+        Tanner had already created a new student position on staff to help get alumni efforts off the
+        ground and was developing a directory of alumni names from archive staff boxes.
+      </p>
+      <p>
+        Working with the Daily Bruin and the UCLA Alumni Association over the next years, the network
+        began to take shape. The DB alumni Facebook group was relaunched and alumni began to step up to
+        join the Committee for the Daily Bruin Alumni Network. The group held its first events in 2018
+        and received official recognition as an affinity network of the UCLA Alumni Association.
+      </p>
+      <p>
+        Many attempts have been made over the past century to build an alumni association for the Daily Bruin.
+        We have taken a number of steps to make this latest incarnation last in perpetuity. For example, The
+        Bruin has committed to a permanent editor position in the student alumni director. By becoming an
+        affinity network of the UCLA Alumni Association, the network has institutional support and expertise
+        to build our numbers, maintain contact information and plan events.
+      </p>
+      <p>
+        We hope you'll join us as we continue to write our history!
+      </p>
+      <h2>NETWORK COMPONENTS</h2>
+      <p>
+        A governing board consisting of Daily Bruin alumni oversees, manages and
+        promotes the alumni network. Committees are in charge of the following projects:
       </p>
       <ul>
         <li>
-          The Daily Bruin Support Fund (#63255O): All alumni can earmark a
-          portion or all of their annual UCLA giving to this official UCLA
-          Office of Scholarships & Student Support Initiatives fund, created
-          specifically to enhance the news-gathering and operational
-          capabilities (such as equipment and travel) at the Daily Bruin.
+          Maintaining a directory for communications, a dedicated DBAN website and an active
+          social media presence. This includes private Facebook and LinkedIn groups for our alumni
+          in all professions to network, reconnect, promote events and to share general updates and
+          career opportunities.
         </li>
         <li>
-          The Daily Bruin Alumni Scholarship & Endowment: Pitch and attract both
-          high-dollar donations and regular alumni support, to establish an
-          endowment to fund an alumni scholarship for student-journalists
-          pursuing original reporting projects of global reach and impact, and
-          to provide additional professional mentoring and advising to better
-          position our student-staffers for the future job market in the media
-          and related fields.
+          Planning an annual on-campus general meeting and reunion, featuring speakers, career panels
+          and receptions for Daily Bruin alumni from all professions, to strengthen bonds among each
+          other and with the current students.
+        </li>
+        <li>
+          Building scholarship programs and fundraising campaigns to support the Daily Bruin and its staffers.
+        </li>
+        <li>
+          Developing a mentorship program and career guidance by alumni in journalism and related fields,
+          including support like advisory workshops and newspaper critiques.
         </li>
       </ul>
-      <h3>The Way Forward</h3>
-      <ul>
-        <li>
-          Spring & Summer 2018 - Recruit and assemble a founding alumni working
-          group to set up the organizational structure and develop programs and
-          benchmarks for the first 2-4 years. With the current Daily Bruin
-          staff, this group would help plan and host the first general meeting
-          and alumni reunion.
-        </li>
-        <li>
-          Fall 2018 - The first annual general meeting and reunion of the Daily
-          Bruin Alumni Network on Friday, October 5, 2018, in Westwood.
-        </li>
-        <li>
-          The Centennial and Beyond: In 2019, the centennial of UCLA and the
-          Daily Bruin, we will work toward gaining official UCLA Alumni
-          Association recognition as an affinity alumni network and fully
-          implement the above-stated goals and objectives.
-        </li>
-      </ul>
+      <p>Please check out our "Volunteer" page to see more.</p>
+      <h2>WHY SUPPORT THE DAILY BRUIN?</h2>
+      <p>
+        The Daily Bruin has had to undergo significant changes in recent years due to steadily- declining advertising
+        revenue. To maintain daily publication, The Bruin has had to seek funding from student fees in 2009 and 2016.
+        To limit rent costs, the Kerckhoff Hall office was reduced to about half its previous size in 2017. The full-time
+        student media advisor position was eliminated, and the rest of the professional staff has been reduced to just three
+        positions. Editor stipends have fallen sharply, requiring many staff members to take additional jobs.
+      </p>
+      <p>
+        The future of journalism depends on the continued success of college newspapers like the Daily Bruin, training the next
+        generation of journalists and media professionals. To see how the DBAN and its members can help The Bruin, please visit
+        our "Give Back" page.
+      </p>
+      <h2>THE WAY FORWARD</h2>
+      <p>
+        In 2019 – the centennial of UCLA and the Daily Bruin – we will work to build our board and committees. With official UCLA
+        Alumni Association recognition as an affinity alumni network, we are prepared to hit the ground running for our goals and objectives.
+      </p>
       <h2>Alumni Spotlight</h2>
       <FeatureQuote
         speaker="Matea Gold"
